bf9079ab2ac30eebc892983e7454c63270cab1da
Jake Bailey <jake@jaketbailey.co.uk>
2024-02-09 14:10:01 +0000

    minor colour alterations

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ export default function Intro() {
     return (
         <div className="flex items-left justify-left flex-col text-left pt-20 pb-6">
          <img src="/jake_portrait.webp" alt="Jake Bailey" className="rounded-full w-48 h-48 mb-3" />
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Jake Bailey</h1>
+         <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Jake Bailey</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Software Engineering Student</p>
          <p className="text-md max-w-full md:max-w-md mb-6">
             I'm an avid cyclist and final-year software engineering student at the University of Portsmouth
